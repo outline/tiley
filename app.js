576f0e64557f44af638fb440f4b4fb7336b844ec
Luke Roberts <luke@sqwiggle.com>
2014-05-29 05:28:39 +0000

    png support

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var morgan  = require('morgan')
+var gm  = require('gm'), imageMagick = gm.subClass({ imageMagick: true });
 var colors = require('./colors.js');
 var argv = require('minimist')(process.argv.slice(2));
 
@@ -19,7 +20,27 @@ app.get('/avatar/:id(\\d+)/:initials(\\w{1,2}).svg', function(req, res){
   var color = defaultPalette[req.params.id % defaultPalette.length] 
   var initials = req.params.initials.toUpperCase();
   res.setHeader("Content-Type", "image/svg+xml");
+  res.setHeader("Vary", "Accept-Encoding");
   res.render('svg', { color:color, text:initials })
+});
+
+app.get('/avatar/:id(\\d+)/:initials(\\w{1,2}).png', function(req, res){
+  res.set('Content-Type', 'image/png');
+  var color = defaultPalette[req.params.id % defaultPalette.length] 
+  var initials = req.params.initials.toUpperCase();
+  // this isnt working but would meab we dont need to read the blank image
+  // imageMagick(100, 100, color). 
+  // read a blank template image
+  imageMagick('fixtures/blank.png').
+  fill(color).
+  drawRectangle(0.0,100,100).
+  fill('#fff').
+  font('fixtures/cabin-regular.ttf', 40).
+  drawText(0, 0, initials, 'Center').
+  stream(function (err, stdout, stderr) {
+    if (err) return next(err);
+    stdout.pipe(res); 
+  });
 });
 
 app.get("/colors", function(req, res){
