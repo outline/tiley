9ada943fda13488e1f76eb49a3f1dce5c2281e1d
Luke Roberts <email@luke-roberts.co.uk>
2014-06-09 11:18:49 +0700

    romaji and jpeg support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan  = require('morgan')
 var gm  = require('gm'), imageMagick = gm.subClass({ imageMagick: true });
 var colors = require('./colors.js');
+var hepburn = require('hepburn')
 var argv = require('minimist')(process.argv.slice(2));
 
 var port = process.env.PORT || parseInt(argv['port'] || 3004);
@@ -11,6 +12,27 @@ var v = parseFloat(argv['v'] || 0.8);
 var seed = parseFloat(argv['seed'] || 0.8);
 var defaultPalette = colors.palette(size, seed, s, v)
 
+var initials = function(source){
+  var result;
+  result = source.toUpperCase();
+  result = hepburn.cleanRomaji(result);
+  result = hepburn.fromKana(result);
+  result = result.substring(0,2);
+  return result;
+}
+
+var generateImage = function(size, color, font, text, format){
+  // this isnt working but would mean we dont need to read the blank image
+  // imageMagick(100, 100, color). 
+  // read a blank template image
+  return imageMagick('fixtures/blank.png').
+  fill(color).
+  drawRectangle(0.0, size, size).
+  fill('#fff').
+  font(font, 40).
+  drawText(-1, -3, text, 'Center').
+  setFormat(format)
+}
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -24,20 +46,14 @@ app.get('/avatar/:id(\\d+)/:initials(\\w{1,2}).svg', function(req, res){
   res.render('svg', { color:color, text:initials })
 });
 
-app.get('/avatar/:id(\\d+)/:initials(\\w{1,2}).png', function(req, res){
-  res.set('Content-Type', 'image/png');
-  var color = defaultPalette[req.params.id % defaultPalette.length] 
-  var initials = req.params.initials.toUpperCase();
-  // this isnt working but would meab we dont need to read the blank image
-  // imageMagick(100, 100, color). 
-  // read a blank template image
-  imageMagick('fixtures/blank.png').
-  fill(color).
-  drawRectangle(0.0,100,100).
-  fill('#fff').
-  font('fixtures/opensans-semibold.ttf', 40).
-  drawText(-1, -3, initials, 'Center').
-  stream(function (err, stdout, stderr) {
+app.get('/avatar/:id(\\d+)/:initials.:format(png|jpg)', function(req, res){
+  var size = 100;
+  var color = defaultPalette[req.params.id % defaultPalette.length];
+  var font = 'fixtures/opensans-semibold.ttf';
+  var text = initials(req.params.initials);
+  var format = req.params.format 
+  res.set('Content-Type', 'image/' + format);
+  generateImage(size, color, font, text, format).stream(function (err, stdout, stderr) {
     if (err) return next(err);
     stdout.pipe(res); 
   });
